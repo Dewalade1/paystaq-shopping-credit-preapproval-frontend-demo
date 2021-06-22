059bce9057f6da3bcc29b34f7fb41f5cab3c322c
Dewa <adewalade@gmail.com>
2021-06-22 13:41:30 +0100

    feat: add page banner on the left

--- components/banner.js
+++ components/banner.js
@@ -1,11 +1,32 @@
-import styles from '../styles/banner.module.css';
+import Link from "next/link";
+
+import styles from "../styles/banner.module.css";
+
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const Banner = () => {
-    return (
-      <div id={styles.bannerLayout}>
-        <p>Banner</p>
+  return (
+    <div id={styles.bannerLayout}>
+      <div id={styles.bannerOverlay}></div>
+      <div id={styles.bannerContent}>
+        <div id={styles.returnLinkContainer}>
+          <Link href="#">
+            <a id={styles.returnLink}>
+              <ArrowBackIcon /> Back to Store
+            </a>
+          </Link>
+        </div>
+
+        <img src="https://res.cloudinary.com/hellodewa/image/upload/v1624359967/paystaq-shopping-credit-preappform-frontend-demo/images/logo-no-bkgd_wjdjof.png" alt="" id={styles.bannerLogo} />
+
+        <ul>
+          <li>Get pre-approved instantly.</li>
+          <li>Spread payment for up to six months.</li>
+          <li>Provide Some basic information to get started.</li>
+        </ul>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Banner;
--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -1,7 +1,11 @@
+import Head from 'next/head';
 
 const Layout = ({children}) => {
     return(
         <>
+        <Head>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+        </Head>
         <main>{children}</main>
         </>)
 }
